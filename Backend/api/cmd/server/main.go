4c13038a35d6d818b2d0d09eceb41a43a2689169
funera1 <funeral7776@gmail.com>
2022-12-14 15:46:41 +0900

    #31 バックエンド開発の基盤を作成

---
+++ Backend/api/cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// sample
+	r.GET("/ping", Ping)
+
+	// sample
+	r.POST("/throw", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		c.JSON(200, gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+
+	r.Run() // 127.0.0.0:8000でサーバを建てる
+}
