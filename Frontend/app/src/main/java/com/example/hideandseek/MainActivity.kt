c5b4eb1d56525501899b560331d7e99ad39e0028
atomic928 <b1020055@fun.ac.jp>
2022-12-14 11:36:54 +0900

    add: MainFragmentの追加
    かくれんぼをするためのメイン画面

--- Frontend/app/src/main/java/com/example/hideandseek/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/hideandseek/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.hideandseek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.hideandseek.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_main
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
 }
---
+++ Frontend/app/src/main/java/com/example/hideandseek/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.hideandseek.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
