8e76f9237c56fea81e82a6b9709249ab9c689d40
funera1 <funeral7776@gmail.com>
2022-12-15 19:46:47 +0900

    host名が間違っていたので修正

--- Backend/api/internal/db/db.go
+++ Backend/api/internal/db/db.go
@@ -4,15 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 
+	_ "github.com/go-sql-driver/mysql"
 	"gopkg.in/gorp.v1"
 )
 
 func InitDb() (*gorp.DbMap, error) {
 	// db接続
 	path := fmt.Sprintf(
-		"%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8&parseTime=true",
+		"%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true",
 		"root",     // username
 		"passwd",   // password
+		"hikky-db", // hostname
 		"hikky-db", // database
 	)
 	db, err := sql.Open("mysql", path)
