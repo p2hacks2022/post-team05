cadc16d68c98fae61c9f712f2b7caa1cdf577cf6
Akira_Maeda <akiramaeda0905@gmail.com>
2022-12-17 15:15:50 +0900

    docsフォルダの追加に伴った変更

--- Web/vue.config.js
+++ Web/vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  publicPath: './Web/'
+  assetsDir: './',
+  publicPath: './'
 })
